eba086699263ad9b4cb1a56328222a8cc9760d13
knc74jEs <knc74jes@emailsmail.com>
2016-10-27 04:29:39 +0000

    Create a project

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in baidu-tuiguang-site.gemspec
+gemspec
---
+++ lib/baidu/tuiguang/site.rb
@@ -0,0 +1,9 @@
+require "baidu/tuiguang/site/version"
+
+module Baidu
+  module Tuiguang
+    module Site
+      # Your code goes here...
+    end
+  end
+end
